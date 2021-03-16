8f30cd724a6fb2d50548d77b7e79acb57ba850ba
deepsey <78343716+deepsey@users.noreply.github.com>
2021-03-16 21:39:44 +0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
+# Возьмем пакет nginx и соберем его с поддержкой openssl
 sudo -i
 cd /root
-echo "Install packets"
+# Устнановим необходимые пакеты
 yum install -y redhat-lsb-core wget rpmdevtools rpm-build createrepo yum-utils gcc
-echo "Wget nginx"
+# Скачаем srpm пакет nginx
 wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm
-echo "Install srm"
+# Создадим в директории /root дерево каталогов для сборки
 rpm -i nginx-1.14.1-1.el7_4.ngx.src.rpm
-echo "Wget openssl"
-wget https://www.openssl.org/source/latest.tar.gz
-tar -xvf latest.tar.gz
-echo "Install dependies"
+#Скачаем последний исходник для openssl и распакуем его
+wget https://www.openssl.org/source/openssl-1.1.1j.tar.gz
+tar -xvf openssl-1.1.1j.tar.gz
+#Установим все зависимости, чтобы избежать ошибок при сборке
 yum-builddep -y rpmbuild/SPECS/nginx.spec
-echo "Copy nginx.spec"
+#Скопируем заранее подготовленный spec файл nginx с нужными опциями
 cp -f /vagrant/nginx.spec /root/rpmbuild/SPECS/
-echo "Build nginx"
+#Собираем nginx и проверяем, что пакеты собрались
 rpmbuild -bb rpmbuild/SPECS/nginx.spec
 ls -l rpmbuild/RPMS/x86_64/
-echo "Install nginx"
+#Устанавливаем nginx и проверяем его запуск
 yum localinstall -y rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm
 systemctl start nginx
 systemctl status nginx
-echo "Mkdir repo"
+#Создаем репозиторий. Создадим каталог repo в директории статики nginx
 mkdir /usr/share/nginx/html/repo
+#Копируем в созданный каталог наш собранный rpm и заранее скаченный rpm для установки репозитория percona-server
 cp rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
-echo "Wget percona"
 cp -f /vagrant/percona-release-0.1-9.noarch.rpm /usr/share/nginx/html/repo/
+#Инициализируем репозиторий
 createrepo /usr/share/nginx/html/repo/
+#Для прозрачности настроим в nginx доступ к листингу каталога:
+sed -i -e 's/location \/ {/location \/ {\nautoindex on;/g' /etc/nginx/conf.d/default.conf
+#Проверяем синтаксис и перезапускаем nginx
 nginx -t
 nginx -s reload
 curl -a http://localhost/repo/
+#Добавим репозиторий в /etc/yum.repos.d
 cat >> /etc/yum.repos.d/otus.repo << EOF
 [otus]
 name=otus-linux
@@ -37,6 +43,8 @@ baseurl=http://localhost/repo
 gpgcheck=0
 enabled=1
 EOF
+#Проверим, что репозиторий подключен и проверим его содержимое
 yum repolist enabled | grep otus
 yum list | grep otus
+# #Установим percona-release
 yum install percona-release -y
